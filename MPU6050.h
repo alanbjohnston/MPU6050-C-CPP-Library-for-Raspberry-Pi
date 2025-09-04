d2a81658a2ea1b04d7b3e9c0b2c9a55aaa5f5af5
Alan Johnston <alan.b.johnston@gmail.com>
2025-09-04 10:34:57 -0400

    Update MPU6050.h set offsets to 0

--- MPU6050.h
+++ MPU6050.h
@@ -25,13 +25,13 @@
 
 //Offsets - supply your own here (calculate offsets with getOffsets function)
 //     Accelerometer
-#define A_OFF_X 19402
-#define A_OFF_Y -2692
-#define A_OFF_Z -8625
+#define A_OFF_X 0 // 19402
+#define A_OFF_Y 0 // -2692
+#define A_OFF_Z 0 // -8625
 //    Gyroscope
-#define G_OFF_X -733
-#define G_OFF_Y 433
-#define G_OFF_Z -75
+#define G_OFF_X 0 // -733
+#define G_OFF_Y 0 // 433
+#define G_OFF_Z 0 // -75
 
 //-----------------------END MODIFY THESE PARAMETERS-----------------------
 
