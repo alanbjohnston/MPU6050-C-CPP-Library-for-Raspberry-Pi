eee25290de81e7264eb79ed48b5b46d3d9ee675a
Alan Johnston <alan.b.johnston@gmail.com>
2025-09-04 12:11:42 -0400

    Update mpu6050.cpp print zeros if not there

--- mpu6050.cpp
+++ mpu6050.cpp
@@ -14,20 +14,26 @@ int main() {
 //	sleep(1); //Wait for the MPU6050 to stabilize
 
 	int f_dev = open("/dev/i2c-1", O_RDWR); //Open the I2C device file
-	std::cout << f_dev << "\n";
+//	std::cout << f_dev << "\n";
 	if (f_dev < 0) { //Catch errors
-		std::cout << "ERR (MPU6050.cpp:MPU6050()): Failed to open /dev/i2c-1. Please check that I2C is enabled with raspi-config\n"; //Print error message
+//		std::cout << "ERR (MPU6050.cpp:MPU6050()): Failed to open /dev/i2c-1. Please check that I2C is enabled with raspi-config\n"; //Print error message
+		std::cout "0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0\n"
+		return(-1);	
 	}	
 	
 	int status = ioctl(f_dev, I2C_SLAVE, 0x68); //Set the I2C bus to use the correct address
-	std::cout << status<< "\n";
+//	std::cout << status<< "\n";
 	if (status < 0) {
-		std::cout << "ERR (MPU6050.cpp:MPU6050()): Could not get I2C bus with 0x68 address. Please confirm that this address is correct\n"; //Print error message
+//		std::cout << "ERR (MPU6050.cpp:MPU6050()): Could not get I2C bus with 0x68 address. Please confirm that this address is correct\n"; //Print error message
+		std::cout "0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0\n"
+		return(-1);	
 	}
 
-	if (i2c_smbus_read_byte_data(f_dev, 0x43) < 0)
-				std::cout << "ERR (MPU6050.cpp:MPU6050()): Could not read I2C bus with 0x68 address. Please confirm that this address is correct\n"; //Print error message
-
+	if (i2c_smbus_read_byte_data(f_dev, 0x43) < 0) {
+//		std::cout << "ERR (MPU6050.cpp:MPU6050()): Could not read I2C bus with 0x68 address. Please confirm that this address is correct\n"; //Print error message
+		std::cout "0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0\n"
+		return(-1);			
+	}
 /*
 	//Calculate the offsets
 	std::cout << "Calculating the offsets...\n    Please keep the accelerometer level and still\n    This could take a couple of minutes...";
