02e5e570aad70b3763e44cfee5ba5a122e7e810f
Alan Johnston <alan.b.johnston@gmail.com>
2025-09-05 13:20:48 -0400

    Update ypr.cpp  3 readings

--- ypr.cpp
+++ ypr.cpp
@@ -47,6 +47,27 @@ int main() {
 //		std::cout << "Current angle around the yaw axis: " << gy << "\n";
 		std::cout << gz << " " <<  gy << " " <<  gx << "\n";
 
+	sleep(1); 
+
+		device.getAngle(0, &gx);
+		device.getAngle(1, &gy);
+		device.getAngle(2, &gz);
+//		std::cout << "Current angle around the roll axis: " << gr << "\n";
+//		std::cout << "Current angle around the pitch axis: " << gp << "\n";
+//		std::cout << "Current angle around the yaw axis: " << gy << "\n";
+		std::cout << gz << " " <<  gy << " " <<  gx << "\n";
+
+	
+	sleep(1); 
+
+		device.getAngle(0, &gx);
+		device.getAngle(1, &gy);
+		device.getAngle(2, &gz);
+//		std::cout << "Current angle around the roll axis: " << gr << "\n";
+//		std::cout << "Current angle around the pitch axis: " << gp << "\n";
+//		std::cout << "Current angle around the yaw axis: " << gy << "\n";
+		std::cout << gz << " " <<  gy << " " <<  gx << "\n";
+	
   	//Get the current accelerometer values
 
 //		usleep(1000000); //1 sec
