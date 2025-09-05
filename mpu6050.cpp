627e82044af04aa8f4c0679f0a18817199cb5b2c
Alan Johnston <alan.b.johnston@gmail.com>
2025-09-05 13:14:31 -0400

    Update mpu6050.cpp only 6 readings

--- mpu6050.cpp
+++ mpu6050.cpp
@@ -19,7 +19,7 @@ int main() {
 //	std::cout << f_dev << "\n";
 	if (f_dev < 0) { //Catch errors
 //		std::cout << "ERR (MPU6050.cpp:MPU6050()): Failed to open /dev/i2c-1. Please check that I2C is enabled with raspi-config\n"; //Print error message
-		std::cout << "0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0\n";
+		std::cout << "0.0 0.0 0.0 0.0 0.0 0.0\n";
 		return(-1);	
 	}	
 	
@@ -27,13 +27,13 @@ int main() {
 //	std::cout << status<< "\n";
 	if (status < 0) {
 //		std::cout << "ERR (MPU6050.cpp:MPU6050()): Could not get I2C bus with 0x68 address. Please confirm that this address is correct\n"; //Print error message
-		std::cout << "0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0\n";
+		std::cout << "0.0 0.0 0.0 0.0 0.0 0.0\n";
 		return(-1);	
 	}
 
 	if (i2c_smbus_read_byte_data(f_dev, 0x43) < 0) {
 //		std::cout << "ERR (MPU6050.cpp:MPU6050()): Could not read I2C bus with 0x68 address. Please confirm that this address is correct\n"; //Print error message
-		std::cout << "0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0\n";
+		std::cout << "0.0 0.0 0.0 0.0 0.0 0.0\n";
 		return(-1);			
 	}
 /*
